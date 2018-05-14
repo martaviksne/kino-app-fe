eb25b4c06199893179a2a558d08418e97b9ac2b9
Marta <marta@Martas-MBP.lan>
2018-05-14 19:02:01 +0300

    polyfill test 2

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import 'core-js/fn/array/find';
 import 'core-js/fn/array/includes';
 import 'core-js/fn/number/is-nan';
 
+import Set from 'es6-set';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
